9eae251d38c3a4c58ffb05e7ff317071d3314949
ayush-shrestha-codes <aayushshresthaa123@gmail.com>
2025-12-03 18:02:28 +0545

    added total returns and identified the best stock

--- predictor.py
+++ predictor.py
@@ -1,3 +1,31 @@
 import numpy as np
-arr_1d = np.array([1, 2, 3, 4, 5])
-print(arr_1d)
+import pandas as pd
+
+np.random.seed(42)
+
+stocks = ["Apple", "Google", "Amazon", "Microsoft", "Tesla"]
+
+num_stocks = len(stocks)  
+num_days = 30              
+initial_prices = np.random.randint(100, 500, size=num_stocks)
+daily_changes = np.random.uniform(-0.05, 0.05, size=(num_days, num_stocks))
+prices = np.zeros((num_days, num_stocks))
+prices[0] = initial_prices
+
+for day in range(1, num_days):
+    prices[day] = prices[day-1] * (1 + daily_changes[day])
+
+df = pd.DataFrame(prices, columns=stocks)
+df.index.name = "Day"
+print(df.head())
+
+returns = df.pct_change().fillna(0)
+print(returns.head())
+
+total_returns=(df.iloc[-1]-df.iloc[0])/df.iloc[0]*100
+print(total_returns.head())
+
+best_stock = total_returns.idxmax()
+worst_stock = total_returns.idxmin()
+print("Best stock:", best_stock)
+print("Worst stock:", worst_stock)
